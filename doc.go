45fb257d69dfc23e1efc6fe34e42bf671cf258bb
Tom Fleet <tomfleet2018@gmail.com>
2023-05-08 14:21:17 +0100

    Add note around nil pointers

--- doc.go
+++ doc.go
@@ -1,2 +1,6 @@
 // Package collections implements a variety of generic data structures e.g. hashsets, stacks and queues.
+//
+// There are 2 general rules for the data structures in this package:
+//   - They should be considered not thread safe unless explicitly stated otherwise.
+//   - They should each be instantiated using the `New“ constructor function, and not directly. Doing so will likely result in a nil pointer dereference.
 package collections
