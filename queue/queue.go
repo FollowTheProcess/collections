48ecabf062ad2284b556a99a2abfecc89558631a
Tom Fleet <tomfleet2018@gmail.com>
2023-11-12 13:55:12 +0000

    Simplify the queue API

--- queue/queue.go
+++ queue/queue.go
@@ -16,27 +16,27 @@ var ErrPopFromEmptyQueue = errors.New("pop from empty queue")
 // A Queue should be instantiated by the New function and not directly,
 // doing so will result in a nil pointer dereference.
 type Queue[T any] struct {
-	container *[]T    // Underlying slice
+	container []T     // Underlying slice
 	options   options // Options for the queue.
 }
 
 // New constructs and returns a new stack.
-func New[T any](options ...Option) Queue[T] {
+func New[T any](options ...Option) *Queue[T] {
 	queue := Queue[T]{}
 	for _, option := range options {
 		option(&queue.options)
 	}
 	container := make([]T, 0, queue.options.capacity)
-	queue.container = &container
-	return queue
+	queue.container = container
+	return &queue
 }
 
 // Push adds an item to the back of the queue.
 //
 //	q := queue.New[string]()
 //	q.Push("hello")
-func (q Queue[T]) Push(item T) {
-	*q.container = append(*q.container, item)
+func (q *Queue[T]) Push(item T) {
+	q.container = append(q.container, item)
 }
 
 // Pop removes an item from the front of the queue, if the queue
@@ -47,14 +47,14 @@ func (q Queue[T]) Push(item T) {
 //	q.Push("there")
 //	item, _ := q.Pop()
 //	fmt.Println(item) // "hello"
-func (q Queue[T]) Pop() (T, error) {
-	l := len(*q.container)
+func (q *Queue[T]) Pop() (T, error) {
+	l := len(q.container)
 	if l == 0 {
 		var none T
 		return none, ErrPopFromEmptyQueue
 	}
-	item := (*q.container)[0]
-	*q.container = (*q.container)[1:]
+	item := (q.container)[0]
+	q.container = (q.container)[1:]
 
 	return item, nil
 }
@@ -66,16 +66,16 @@ func (q Queue[T]) Pop() (T, error) {
 //	s.Push("hello")
 //	s.Push("there")
 //	s.Length() // 2
-func (q Queue[T]) Length() int {
-	return len(*q.container)
+func (q *Queue[T]) Length() int {
+	return len(q.container)
 }
 
 // Cap returns the current capacity of the queue.
 //
 //	s := queue.New[string](queue.WithCapacity(10))
 //	s.Cap() // 10
-func (q Queue[T]) Cap() int {
-	return cap(*q.container)
+func (q *Queue[T]) Cap() int {
+	return cap(q.container)
 }
 
 // IsEmpty returns whether or not the queue is empty.
@@ -84,8 +84,8 @@ func (q Queue[T]) Cap() int {
 //	s.IsEmpty() // true
 //	s.Push("hello")
 //	s.IsEmpty() // false
-func (q Queue[T]) IsEmpty() bool {
-	return len(*q.container) == 0
+func (q *Queue[T]) IsEmpty() bool {
+	return len(q.container) == 0
 }
 
 // Items returns the items in the queue as a new slice (copy).
@@ -94,11 +94,11 @@ func (q Queue[T]) IsEmpty() bool {
 //	q.Push("hello")
 //	q.Push("there")
 //	q.Items() // [hello there]
-func (q Queue[T]) Items() []T {
-	return append([]T{}, *q.container...)
+func (q *Queue[T]) Items() []T {
+	return append([]T{}, q.container...)
 }
 
 // String satisfies the stringer interface and allows a stack to be printed.
-func (q Queue[T]) String() string {
-	return fmt.Sprintf("%v", *q.container)
+func (q *Queue[T]) String() string {
+	return fmt.Sprintf("%v", q.container)
 }
