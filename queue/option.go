014679f575cdbe9aa940b52b895291afb4264b9c
Tom Fleet <tomfleet2018@gmail.com>
2023-11-12 13:57:20 +0000

    Simplify the queue API (#33)

--- queue/option.go
+++ queue/option.go
@@ -10,7 +10,8 @@ type Option func(*options)
 
 // WithCapacity sets the initial capacity of the queue.
 //
-// If the capacity is negative, the default of 0 is used.
+// If the capacity is negative, the default of 0 is used which is equivalent to
+// the capacity go allocates new slices.
 //
 //	q := queue.New[string](queue.WithCapacity(10))
 //	q.Cap() // 10
