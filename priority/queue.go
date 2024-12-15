831f78395837f1844259f6e77f8fa16bffc27705
Tom Fleet <tomfleet2018@gmail.com>
2024-12-15 13:11:50 +0000

    Make `Element` a public member in priority queue and add `From` and `FromFunc`

--- priority/queue.go
+++ priority/queue.go
@@ -2,27 +2,79 @@
 // and are popped off the queue in (descending) order of this priority.
 package priority
 
-import "errors"
+import (
+	"errors"
+)
 
-// element holds an element in the priority queue along with it's priority.
-type element[T any] struct {
-	item     T   // The stored item
-	priority int // The priority of the element
+// Element holds an element in the priority queue along with it's priority.
+type Element[T any] struct {
+	Item     T   // The stored item itself
+	Priority int // The priority of the element, highest gets popped first
 }
 
 // Queue is a generic priority queue.
 type Queue[T any] struct {
-	container []element[T] // Underlying slice
+	container []Element[T] // Underlying slice
 }
 
 // New builds and returns a new, empty priority Queue.
+//
+// If you already have a list of items you wish to transform into a priority queue,
+// consider using [From] or [FromFunc] as they are more performant than constructing
+// and empty queue and filling it in a loop.
 func New[T any]() *Queue[T] {
 	return &Queue[T]{}
 }
 
+// From builds and returns a priority Queue from an already established []Element.
+//
+// This is more performant than creating a new empty Queue and using Push, but requires
+// the caller to construct the slice of Element themselves.
+func From[T any](elements []Element[T]) *Queue[T] {
+	queue := &Queue[T]{
+		container: make([]Element[T], 0, len(elements)), // Create a new slice so we own the data internally
+	}
+
+	queue.container = append(queue.container, elements...)
+
+	// Heapify the container
+	queue.init()
+
+	return queue
+}
+
+// FromFunc builds and returns a priority Queue from an already established list of items, taking
+// a closure that is called to calculate the priority of each item.
+//
+// This is more performant than creating a new empty Queue and using Push, but requires
+// the caller to construct the slice of items themselves.
+//
+// It is slightly less performant than calling [From] as the priority is calculated
+// on the fly.
+//
+// Take care implementing the priorityFunc as it is called for every element in items, it should
+// be as performant as possible.
+func FromFunc[T any](items []T, priorityFunc func(item T) int) *Queue[T] {
+	container := make([]Element[T], 0, len(items))
+	for _, item := range items {
+		container = append(container, Element[T]{Item: item, Priority: priorityFunc(item)})
+	}
+
+	queue := &Queue[T]{
+		container: container,
+	}
+	// Heapify the container
+	queue.init()
+
+	return queue
+}
+
 // Push adds an item and it's priority to the queue.
+//
+// If you already have an existing slice of items, consider converting it to []Element
+// and using [From] as it is more performant.
 func (q *Queue[T]) Push(item T, priority int) {
-	q.container = append(q.container, element[T]{item: item, priority: priority})
+	q.container = append(q.container, Element[T]{Item: item, Priority: priority})
 	q.siftUp(len(q.container) - 1)
 }
 
@@ -44,7 +96,7 @@ func (q *Queue[T]) Pop() (T, error) {
 	// Update heap order
 	q.siftDown(0, n)
 
-	return elem.item, nil
+	return elem.Item, nil
 }
 
 // Size returns the number of elements currently in the queue.
@@ -57,12 +109,21 @@ func (q *Queue[T]) Empty() bool {
 	return len(q.container) == 0
 }
 
+// init heapifies the underlying container, establishing the heap invariants required by
+// the other methods. It is only used when creating a priority queue with [From].
+func (q *Queue[T]) init() {
+	n := len(q.container)
+	for i := n/2 - 1; i >= 0; i-- { //nolint: mnd
+		q.siftDown(i, n)
+	}
+}
+
 // siftUp moves an item (by index) up the heap until it's in the correct position
 // for it's priority.
 func (q *Queue[T]) siftUp(index int) {
 	for {
 		parent := (index - 1) / 2 //nolint: mnd
-		if parent == index || q.container[index].priority < q.container[parent].priority {
+		if parent == index || q.container[index].Priority < q.container[parent].Priority {
 			break
 		}
 		q.swap(parent, index)
@@ -98,5 +159,5 @@ func (q *Queue[T]) swap(i, j int) {
 
 // less reports whether element i should come before element j in priority order.
 func (q *Queue[T]) less(i, j int) bool {
-	return q.container[i].priority > q.container[j].priority
+	return q.container[i].Priority > q.container[j].Priority
 }
--- priority/queue_test.go
+++ priority/queue_test.go
@@ -17,6 +17,56 @@ func TestNew(t *testing.T) {
 	test.Equal(t, item, "")
 }
 
+func TestFrom(t *testing.T) {
+	items := []priority.Element[string]{
+		{Item: "six", Priority: 6},
+		{Item: "one", Priority: 1},
+		{Item: "three", Priority: 3},
+		{Item: "four", Priority: 4},
+		{Item: "two", Priority: 2},
+		{Item: "five", Priority: 5},
+	}
+
+	q := priority.From(items)
+
+	test.Equal(t, q.Size(), 6)
+
+	item, err := q.Pop()
+	test.Ok(t, err)
+	test.Equal(t, item, "six")
+}
+
+func TestFromFunc(t *testing.T) {
+	items := []string{"one", "two", "three", "four", "five", "six"}
+
+	priorityFunc := func(item string) int {
+		switch item {
+		case "one":
+			return 1
+		case "two":
+			return 2
+		case "three":
+			return 3
+		case "four":
+			return 4
+		case "five":
+			return 5
+		case "six":
+			return 6
+		default:
+			return 0
+		}
+	}
+
+	q := priority.FromFunc(items, priorityFunc)
+
+	test.Equal(t, q.Size(), 6)
+
+	item, err := q.Pop()
+	test.Ok(t, err)
+	test.Equal(t, item, "six")
+}
+
 func TestSize(t *testing.T) {
 	q := priority.New[string]()
 	test.Equal(t, q.Size(), 0)     // Initial size should be empty
@@ -62,4 +112,64 @@ func TestPushPop(t *testing.T) {
 	fifth, err := q.Pop()
 	test.Err(t, err) // Pop from empty queue
 	test.Equal(t, fifth, "")
+}
+
+// BenchmarkNew measures the performance of constructing a new empty Queue
+// and calling Push to fill it with elements.
+func BenchmarkNew(b *testing.B) {
+	for range b.N {
+		q := priority.New[string]()
+		q.Push("one", 1)
+		q.Push("two", 2)
+		q.Push("three", 3)
+		q.Push("four", 4)
+		q.Push("five", 5)
+		q.Push("six", 6)
+	}
+}
+
+// BenchmarkFrom measures the performance of constructing a priority Queue
+// from a pre-existing slice of Elements.
+func BenchmarkFrom(b *testing.B) {
+	elements := []priority.Element[string]{
+		{Item: "six", Priority: 6},
+		{Item: "one", Priority: 1},
+		{Item: "three", Priority: 3},
+		{Item: "four", Priority: 4},
+		{Item: "two", Priority: 2},
+		{Item: "five", Priority: 5},
+	}
+
+	for range b.N {
+		priority.From(elements)
+	}
+}
+
+// BenchmarkFromFunc measures the performance of constructing a priority Queue
+// from a pre-existing slice of items and calculating the priority with a closure.
+func BenchmarkFromFunc(b *testing.B) {
+	items := []string{"one", "two", "three", "four", "five", "six"}
+
+	priorityFunc := func(item string) int {
+		switch item {
+		case "one":
+			return 1
+		case "two":
+			return 2
+		case "three":
+			return 3
+		case "four":
+			return 4
+		case "five":
+			return 5
+		case "six":
+			return 6
+		default:
+			return 0
+		}
+	}
+
+	for range b.N {
+		priority.FromFunc(items, priorityFunc)
+	}
 }
